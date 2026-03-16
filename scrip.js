493a79269fd5370ad414445c8f74c5d6436d8875
Andre246092 <andrecardoso2@hotmail.com>
2026-03-16 13:15:49 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -2,35 +2,28 @@ const scriptURL = "https://script.google.com/macros/s/AKfycbypDWV-viQPJlSXBz1J65
 
 const form = document.getElementById("form")
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", e => {
 
 e.preventDefault()
 
-const formData = new FormData()
-
-formData.append("apto", document.getElementById("apto").value)
-formData.append("nome", document.getElementById("nome").value)
-formData.append("telefone", document.getElementById("telefone").value)
-formData.append("email", document.getElementById("email").value)
-formData.append("emergencia", document.getElementById("emergencia").value)
-formData.append("telEmergencia", document.getElementById("telEmergencia").value)
+const formData = new FormData(form)
 
 fetch(scriptURL, {
 method: "POST",
 body: formData
 })
+.then(response => response.text())
 .then(() => {
 
-alert("Cadastro enviado com sucesso!")
+alert("Cadastro enviado com sucesso")
 
 form.reset()
 
 })
 .catch(error => {
 
-alert("Erro ao enviar cadastro")
-
 console.error(error)
+alert("Erro ao enviar cadastro")
 
 })
 
